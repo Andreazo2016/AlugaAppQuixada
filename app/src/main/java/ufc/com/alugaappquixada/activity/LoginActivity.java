18f95e4d9bc700530cd7a75297d7fe2a32c3dee6
andreazoDevman <andreazo2012@gmail.com>
2018-10-28 14:41:06 -0300

    feito a tela login do app

---
+++ app/src/main/java/ufc/com/alugaappquixada/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package ufc.com.alugaappquixada.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ufc.com.alugaappquixada.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
