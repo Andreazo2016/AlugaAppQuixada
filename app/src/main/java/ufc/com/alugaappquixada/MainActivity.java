8e44451faa0cb10749e5aa66f38aa7ead6336e8d
andreazoDevman <andreazo2012@gmail.com>
2018-10-30 16:24:45 -0300

    adicionado o google maps ao projeto

--- app/src/main/java/ufc/com/alugaappquixada/MainActivity.java
+++ app/src/main/java/ufc/com/alugaappquixada/MainActivity.java
@@ -1,7 +1,10 @@
 package ufc.com.alugaappquixada;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import ufc.com.alugaappquixada.activity.HomeActivity;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this,HomeActivity.class));
     }
 }
